22947fd23691945422d6e748a0ee0ce309a2d98e
Osadch <122278218+Osadch@users.noreply.github.com>
2023-06-05 16:26:31 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,12 @@
-// место для вашего кода
+// напишите решение с нуля
+// код сохраните в свой git-репозиторий
+#include "stat_reader.h"
+#include "input_reader.h"
+
+using namespace transport;
+
+int main() {
+    TransportCatalogue catalogue;
+    FillTransportCatalogue(catalogue);
+    ProcessRequests(catalogue);
+}
