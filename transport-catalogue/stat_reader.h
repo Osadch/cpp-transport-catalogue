cb2807693ee67468337d3ccdab29c1d55fd1c45b
Osadch <122278218+Osadch@users.noreply.github.com>
2023-06-05 16:27:45 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,14 @@
-// место для вашего кода
+#pragma once
+// напишите решение с нуля
+// код сохраните в свой git-репозиторий
+#include "transport_catalogue.h"
+
+#include <iostream>
+
+namespace transport {
+
+void ProcessRequests(TransportCatalogue& catalogue);
+void PrintRoute(std::string& line, TransportCatalogue& catalogue);
+void PrintStop(std::string& line, TransportCatalogue& catalogue);
+
+} // namespace transport
