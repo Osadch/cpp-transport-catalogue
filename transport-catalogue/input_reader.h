f7691df10e0533b2d89921a55dfe21c516d9f5fe
Osadch <122278218+Osadch@users.noreply.github.com>
2023-06-06 13:33:21 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -6,12 +6,13 @@
 #include "transport_catalogue.h"
 #include <iostream>
 
+
 namespace transport {
 
-void FillTransportCatalogue(TransportCatalogue& catalogue);
-Bus FillRoute(std::string& line);
-Stop FillStop(std::string& line);
-void AddStopDistances(std::string& line, TransportCatalogue& catalogue);
+	void FillCatalogue(std::istream& in, Catalogue& catalogue);
+
+	std::pair<std::string, geo::Coordinates> FillStop(std::string& line);
+	void FillStopDistances(std::string& line, Catalogue& catalogue);
+	std::tuple<std::string, std::vector<const Stop*>, bool> FillRoute(std::string& line, transport::Catalogue& catalogue);
 
 } // namespace transport
-
