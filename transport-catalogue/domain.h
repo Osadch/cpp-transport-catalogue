a93c7475b4e5dd0d8b545327b6a06ad2c817f7a1
Osadch <122278218+Osadch@users.noreply.github.com>
2023-06-20 17:03:55 +0300

    Update domain.h

--- transport-catalogue/domain.h
+++ transport-catalogue/domain.h
@@ -1,17 +1,5 @@
 #pragma once
 
-/*
- * В этом файле вы можете разместить классы/структуры, которые являются частью предметной области (domain)
- * вашего приложения и не зависят от транспортного справочника. Например Автобусные маршруты и Остановки. 
- *
- * Их можно было бы разместить и в transport_catalogue.h, однако вынесение их в отдельный
- * заголовочный файл может оказаться полезным, когда дело дойдёт до визуализации карты маршрутов:
- * визуализатор карты (map_renderer) можно будет сделать независящим от транспортного справочника.
- *
- * Если структура вашего приложения не позволяет так сделать, просто оставьте этот файл пустым.
- *
- */
-
 #include "geo.h"
 
 #include <string>
@@ -21,23 +9,24 @@
 
 namespace transport {
 
-struct Stop {
-    std::string name;
-    geo::Coordinates coordinates;
-    std::set<std::string> buses_by_stop;
-};
+    struct Stop {
+        std::string name;
+        geo::Coordinates coordinates;
+        std::set<std::string> buses_by_stop;
+    };
 
-struct Bus {
-    std::string number;
-    std::vector<const Stop*> stops;
-    bool is_circle;
-};
+    struct Bus {
+        std::string number;
+        std::vector<const Stop*> stops;
+        bool is_circle;
+    };
 
-struct BusStat {
-    size_t stops_count;
-    size_t unique_stops_count;
-    double route_length;
-    double curvature;
-};
+    struct BusStat {
+        size_t stops_count;
+        size_t unique_stops_count;
+        double route_length;
+        double curvature;
+    };
 
+   
 } // namespace transport
