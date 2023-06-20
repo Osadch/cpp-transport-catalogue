775a343292694a687a9700f75afacdff980970d6
Osadch <122278218+Osadch@users.noreply.github.com>
2023-06-20 17:10:52 +0300

    Update json_reader.h

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -1,9 +1,5 @@
 #pragma once
 
-/*
- * Здесь можно разместить код наполнения транспортного справочника данными из JSON,
- * а также код обработки запросов к базе и формирование массива ответов в формате JSON
- */
 #include "json.h"
 #include "transport_catalogue.h"
 #include "map_renderer.h"
@@ -33,7 +29,7 @@ public:
 private:
     json::Document input_;
     json::Node dummy_ = nullptr;
-
+   
     std::tuple<std::string_view, geo::Coordinates, std::map<std::string_view, int>> FillStop(const json::Dict& request_map) const;
     void FillStopDistances(transport::Catalogue& catalogue) const;
     std::tuple<std::string_view, std::vector<const transport::Stop*>, bool> FillRoute(const json::Dict& request_map, transport::Catalogue& catalogue) const;
