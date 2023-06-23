0685718c1842fb2ab3247e6e24a47d2ac5adb46e
Osadch <122278218+Osadch@users.noreply.github.com>
2023-06-23 12:29:19 +0300

    Update request_handler.h

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -6,22 +6,31 @@
 
 #include <sstream>
 
-class RequestHandler {
-public:
-    RequestHandler(const transport::Catalogue& catalogue, const renderer::MapRenderer& renderer)
-        : catalogue_(catalogue)
-        , renderer_(renderer)
-    {
-    }
 
-    std::optional<transport::BusStat> GetBusStat(const std::string_view bus_number) const;
-    const std::set<std::string> GetBusesByStop(std::string_view stop_name) const;
-    bool IsBusNumber(const std::string_view bus_number) const;
-    bool IsStopName(const std::string_view stop_name) const;
+ class RequestHandler {
+ public:
+     // MapRenderer понадобится в следующей части итогового проекта
+     RequestHandler(const transport::TransportCatalogue& catalogue, const renderer::MapRenderer& renderer)
+         : catalogue_(catalogue)
+         , renderer_(renderer)
+     {
+     }
 
-    svg::Document RenderMap() const;
+     // Возвращает информацию о маршруте (запрос Bus)
+     std::optional<transport::BusStat> GetBusStat(const std::string_view bus_number) const;
+     // Возвращает маршруты, проходящие через
+     const std::set<std::string> GetBusesByStop(std::string_view stop_name) const;
 
-private:
-    const transport::Catalogue& catalogue_;
-    const renderer::MapRenderer& renderer_;
-};
+     bool IsBusNumber(const std::string_view bus_number) const;
+
+     bool IsStopName(const std::string_view stop_name) const;
+
+     // Этот метод будет нужен в следующей части итогового проекта
+     svg::Document RenderMap() const;
+
+ private:
+     // RequestHandler использует агрегацию объектов "Транспортный Справочник" и "Визуализатор Карты"
+     const transport::TransportCatalogue& catalogue_;
+     const renderer::MapRenderer& renderer_;
+ };
+ 
