6ba8a40a50611962a0b65a3a023cf50c35dd251f
Osadch <122278218+Osadch@users.noreply.github.com>
2023-06-23 12:25:10 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -2,11 +2,11 @@
 #include "request_handler.h"
 
 int main() {
-    transport::Catalogue catalogue;
+    transport::TransportCatalogue catalogue;
     JsonReader json_doc(std::cin);
-    
+
     json_doc.FillCatalogue(catalogue);
-    
+
     const auto& stat_requests = json_doc.GetStatRequests();
     const auto& render_settings = json_doc.GetRenderSettings().AsMap();
     const auto& renderer = json_doc.FillRenderSettings(render_settings);
