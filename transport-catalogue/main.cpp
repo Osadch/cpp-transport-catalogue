7a6a7083a9814777ed393d3ac0ec678e7c50d76b
Osadch <122278218+Osadch@users.noreply.github.com>
2023-07-30 17:02:11 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -8,9 +8,16 @@ int main() {
     json_doc.FillCatalogue(catalogue);
 
     const auto& stat_requests = json_doc.GetStatRequests();
-    const auto& render_settings = json_doc.GetRenderSettings().AsDict();
+    const auto& render_settings = json_doc.GetRenderSettings();
     const auto& renderer = json_doc.FillRenderSettings(render_settings);
+    const auto& routing_settings = json_doc.FillRoutingSettings(json_doc.GetRoutingSettings());
+    const transport::Router router = { routing_settings, catalogue };
 
-    RequestHandler rh(catalogue, renderer);
+    //const auto& stat_requests = json_doc.GetStatRequests();
+    //const auto& render_settings = json_doc.GetRenderSettings().AsDict();
+    //const auto& renderer = json_doc.FillRenderSettings(render_settings);
+
+    RequestHandler rh(catalogue, renderer, router);
+    //RequestHandler rh(catalogue, renderer);
     json_doc.ProcessRequests(stat_requests, rh);
 }
