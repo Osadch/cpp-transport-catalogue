e98f0b6c636aea054d5ceba13c8835d86f7ca3ec
Osadch <122278218+Osadch@users.noreply.github.com>
2023-07-30 17:04:58 +0300

    Update request_handler.h

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -3,16 +3,19 @@
 #include "json.h"
 #include "transport_catalogue.h"
 #include "map_renderer.h"
+#include "transport_router.h"
 
 #include <sstream>
+#include <optional>
 
 
  class RequestHandler {
  public:
      // MapRenderer понадобится в следующей части итогового проекта
-     RequestHandler(const transport::TransportCatalogue& catalogue, const renderer::MapRenderer& renderer)
+     RequestHandler(const transport::TransportCatalogue& catalogue, const renderer::MapRenderer& renderer, const transport::Router& router)
          : catalogue_(catalogue)
          , renderer_(renderer)
+         , router_(router)
      {
      }
 
@@ -27,10 +30,16 @@
 
      // Этот метод будет нужен в следующей части итогового проекта
      svg::Document RenderMap() const;
+        
+    
+     const std::optional<graph::Router<double>::RouteInfo> GetOptimalRoute(const std::string_view stop_from, const std::string_view stop_to) const;
+     const graph::DirectedWeightedGraph<double>& GetRouterGraph() const;
+
+     
 
  private:
      // RequestHandler использует агрегацию объектов "Транспортный Справочник" и "Визуализатор Карты"
      const transport::TransportCatalogue& catalogue_;
      const renderer::MapRenderer& renderer_;
+     const transport::Router& router_;
  };
- 
