fff139734af1b8b5be2f6b2206e0a8fe4751fe69
Osadch <122278218+Osadch@users.noreply.github.com>
2023-07-30 17:06:24 +0300

    Update transport_catalogue.cpp

--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -9,7 +9,7 @@ namespace transport {
         stopname_to_stop_[all_stops_.back().name] = &all_stops_.back();
     }
 
-    void TransportCatalogue::AddRoute(std::string_view bus_number, const std::vector<const Stop*> stops, bool is_circle) {
+      void TransportCatalogue::AddRoute(std::string_view bus_number, const std::vector<const Stop*> stops, bool is_circle) {
         all_buses_.push_back({ std::string(bus_number), stops, is_circle });
         busname_to_bus_[all_buses_.back().number] = &all_buses_.back();
         for (const auto& route_stop : stops) {
@@ -19,6 +19,7 @@ namespace transport {
         }
     }
 
+
     const Bus* TransportCatalogue::FindRoute(std::string_view bus_number) const {
         return busname_to_bus_.count(bus_number) ? busname_to_bus_.at(bus_number) : nullptr;
     }
@@ -27,6 +28,7 @@ namespace transport {
         return stopname_to_stop_.count(stop_name) ? stopname_to_stop_.at(stop_name) : nullptr;
     }
 
+  
     size_t TransportCatalogue::UniqueStopsCount(std::string_view bus_number) const {
         std::unordered_set<std::string_view> unique_stops;
         for (const auto& stop : busname_to_bus_.at(bus_number)->stops) {
@@ -35,16 +37,24 @@ namespace transport {
         return unique_stops.size();
     }
 
+    
     void TransportCatalogue::SetDistance(const Stop* from, const Stop* to, const int distance) {
         stop_distances_[{from, to}] = distance;
     }
 
     int TransportCatalogue::GetDistance(const Stop* from, const Stop* to) const {
-        if (stop_distances_.count({ from, to })) return stop_distances_.at({ from, to });
-        else if (stop_distances_.count({ to, from })) return stop_distances_.at({ to, from });
-        else return 0;
+        if (stop_distances_.count({ from, to })) {
+            return stop_distances_.at({ from, to });
+        }
+        else if (stop_distances_.count({ to, from })) {
+            return stop_distances_.at({ to, from });
+        }
+        else {
+            return 0;
+        }
     }
 
+  
     const std::map<std::string_view, const Bus*> TransportCatalogue::GetSortedAllBuses() const {
         std::map<std::string_view, const Bus*> result;
         for (const auto& bus : busname_to_bus_) {
@@ -53,33 +63,14 @@ namespace transport {
         return result;
     }
 
-    std::optional<transport::BusStat> TransportCatalogue::GetBusStat(const std::string_view bus_number) const {
-        transport::BusStat bus_stat{};
-        const transport::Bus* bus = FindRoute(bus_number);
-        if (!bus) throw std::invalid_argument("bus not found");
-        if (bus->is_circle) bus_stat.stops_count = bus->stops.size();
-        else bus_stat.stops_count = bus->stops.size() * 2 - 1;
-        int route_length = 0;
-        double geographic_length = 0.0;
-        for (size_t i = 0; i < bus->stops.size() - 1; ++i) {
-            auto from = bus->stops[i];
-            auto to = bus->stops[i + 1];
-            if (bus->is_circle) {
-                route_length += GetDistance(from, to);
-                geographic_length += geo::ComputeDistance(from->coordinates,
-                    to->coordinates);
-            }
-            else {
-                route_length += GetDistance(from, to) + GetDistance(to, from);
-                geographic_length += geo::ComputeDistance(from->coordinates,
-                    to->coordinates) * 2;
-            }
+       const std::map<std::string_view, const Stop*> TransportCatalogue::GetSortedAllStops() const {
+        std::map<std::string_view, const Stop*> result;
+        for (const auto& stop : stopname_to_stop_) {
+            result.emplace(stop);
         }
-        bus_stat.unique_stops_count = UniqueStopsCount(bus_number);
-        bus_stat.route_length = route_length;
-        bus_stat.curvature = route_length / geographic_length;
-        return bus_stat;
+        return result;
     }
-   
+
+       
 
 } // namespace transport
