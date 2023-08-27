7e86bd62c41c52f60108c7b800b42e0b98f7d61c
Osadch <122278218+Osadch@users.noreply.github.com>
2023-08-27 12:57:06 +0300

    Update geo.h

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -1,18 +1,20 @@
 #pragma once
 
+#include <cmath>
+ 
 namespace geo {
-
-    struct Coordinates {
-        double lat; 
-        double lng; 
-        bool operator==(const Coordinates& other) const {
-            return lat == other.lat && lng == other.lng;
-        }
-        bool operator!=(const Coordinates& other) const {
-            return !(*this == other);
-        }
-    };
-
-    double ComputeDistance(Coordinates from, Coordinates to);
-
-}  // namespace geo
+    
+struct Coordinates {
+    double latitude; // Широта
+    double longitude; // Долгота
+ 
+    bool operator==(const Coordinates& other) const {
+        return latitude == other.latitude && longitude == other.longitude;
+    }
+    bool operator!=(const Coordinates& other) const {
+        return !(*this == other);
+    }
+};    
+double compute_distance(Coordinates start, Coordinates end);
+    
+}//end namespace geo
