e87566f19ca641d1ce74112e43b48fb670d18d07
Osadch <122278218+Osadch@users.noreply.github.com>
2023-08-27 23:17:54 +0300

    Update geo.cpp

--- transport-catalogue/geo.cpp
+++ transport-catalogue/geo.cpp
@@ -1,21 +1,20 @@
 #define _USE_MATH_DEFINES
 #include "geo.h"
- 
+
 #include <cmath>
- 
+
 namespace geo {
-    
-double compute_distance(Coordinates start, Coordinates end) {
-    using namespace std;
-    
-    if (!(start == end)) {
-        const double dr = M_PI / 180.;
-        return acos(sin(start.latitude * dr) * sin(end.latitude * dr)
-                    + cos(start.latitude * dr) * cos(end.latitude * dr) 
-                    * cos(abs(start.longitude - end.longitude) * dr)) * 6371000;
-    } else {
-        return 0.0;
+
+    double ComputeDistance(Coordinates from, Coordinates to) {
+        using namespace std;
+        if (from == to) {
+            return 0;
+        }
+        static const double dr = M_PI / 180.;
+        static const int radius = 6371000;
+        return acos(sin(from.lat * dr) * sin(to.lat * dr)
+            + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
+            * radius;
     }
+
 }
-    
-}//end namespace geo
