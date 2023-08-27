fbd4dd5e362b8b282ec9af7a61b50fb3a18f99c5
Osadch <122278218+Osadch@users.noreply.github.com>
2023-08-27 13:21:30 +0300

    Update transport_router.h

--- transport-catalogue/transport_router.h
+++ transport-catalogue/transport_router.h
@@ -1,41 +1,71 @@
 #pragma once
-
-#include "router.h"
+ 
 #include "transport_catalogue.h"
-
-#include <memory>
-
-namespace transport {
-
-	class Router {
-	public:
-		Router() = default;
-
-		Router(const int bus_wait_time, const double bus_velocity)
-			: bus_wait_time_(bus_wait_time)
-			, bus_velocity_(bus_velocity) {}
-
-		Router(const Router& settings, const TransportCatalogue& catalogue) {
-			bus_wait_time_ = settings.bus_wait_time_;
-			bus_velocity_ = settings.bus_velocity_;
-			BuildGraph(catalogue); 
-		}
-		// Метод создания графа
-		const graph::DirectedWeightedGraph<double>& BuildGraph(const TransportCatalogue& catalogue);
-		// Метод поиска маршрута
-		const std::optional<graph::Router<double>::RouteInfo> FindRoute(const std::string_view stop_from, const std::string_view stop_to) const;
-		// Метод получения графа
-		const graph::DirectedWeightedGraph<double>& GetGraph() const;
-
-	private:
-		int bus_wait_time_ = 0;
-		double bus_velocity_ = 0.0;
-
-		graph::DirectedWeightedGraph<double> graph_;
-		// map идентификаторов остановок
-		std::map<std::string, graph::VertexId> stop_ids_;
-		// Указатель на объект класса Router
-		std::unique_ptr<graph::Router<double>> router_;
-	};
-
-}
+#include "router.h"
+#include "domain.h"
+ 
+#include <deque>
+#include <unordered_map>
+#include <iostream>
+ 
+namespace transport_catalogue {
+namespace detail {
+namespace router {
+ 
+using namespace domain;
+using namespace graph;
+ 
+ class TransportRouter {
+public:
+    void set_routing_settings(RoutingSettings routing_settings);
+    const RoutingSettings& get_routing_settings() const; 
+    void build_router(TransportCatalogue& transport_catalogue); 
+    const DirectedWeightedGraph<double>& get_graph() const;
+    const Router<double>& get_router() const;
+    const std::variant<StopEdge, BusEdge>& get_edge(EdgeId id) const;    
+    std::optional<RouterByStop> get_router_by_stop(Stop* stop) const;
+    std::optional<RouteInfo> get_route_info(VertexId start, VertexId end) const; 
+    const std::unordered_map<Stop*, RouterByStop>& get_stop_to_vertex() const;
+    const std::unordered_map<EdgeId, std::variant<StopEdge, BusEdge>>& get_edge_id_to_edge() const;    
+    std::deque<Stop*> get_stops_ptr(TransportCatalogue& transport_catalogue);
+    std::deque<Bus*> get_bus_ptr(TransportCatalogue& transport_catalogue);        
+    void add_edge_to_stop();
+    void add_edge_to_bus(TransportCatalogue& transport_catalogue);    
+    void set_stops(const std::deque<Stop*>& stops);
+    void set_graph(TransportCatalogue& transport_catalogue); 
+    Edge<double> make_edge_to_bus(Stop* start, Stop* end, const double distance) const;
+ 
+    template <typename Iterator>
+    void parse_bus_to_edges(Iterator first, 
+                            Iterator last,
+                            const TransportCatalogue& transport_catalogue, 
+                            const Bus* bus);
+    
+private:    
+    std::unordered_map<Stop*, RouterByStop> stop_to_router_;
+    std::unordered_map<EdgeId, std::variant<StopEdge, BusEdge>> edge_id_to_edge_;    
+    std::unique_ptr<DirectedWeightedGraph<double>> graph_;
+    std::unique_ptr<Router<double>> router_;    
+    RoutingSettings routing_settings_;
+};
+ 
+template <typename Iterator>
+void TransportRouter::parse_bus_to_edges(Iterator first, 
+                                         Iterator last,
+                                         const TransportCatalogue& transport_catalogue, 
+                                         const Bus* bus) {
+    
+    for (auto it = first; it != last; ++it) {
+        size_t distance = 0;
+        size_t span = 0; 
+        for (auto it2 = std::next(it); it2 != last; ++it2) {
+            distance += transport_catalogue.get_distance_stop(*prev(it2), *it2);
+            ++span; 
+            EdgeId id = graph_->add_edge(make_edge_to_bus(*it, *it2, distance));            
+            edge_id_to_edge_[id] = BusEdge{bus->name, span, graph_->get_edge(id).weight};
+        }
+    }
+} 
+}//end namespace router
+}//end namespace detail
+}//end namespace transport_catalogue
