aad17f687487327173ace61988cb2f64f1c3a087
Osadch <122278218+Osadch@users.noreply.github.com>
2023-08-28 09:51:15 +0300

    Update serialization.h

--- transport-catalogue/serialization.h
+++ transport-catalogue/serialization.h
@@ -10,20 +10,20 @@
 
 #include <transport_catalogue.pb.h>
 
-void Serialize(const transport::Catalogue& tcat,
+void SerializeCatalogue(const transport::Catalogue& catalogue,
     const renderer::MapRenderer& renderer,
     const transport::Router& router,
     std::ostream& output);
 
-serialize::Stop Serialize(const transport::Stop* stop);
+serialize::Stop SerializeStop(const transport::Stop* stop);
 
-serialize::Bus Serialize(const transport::Bus* bus);
+serialize::Bus SerializeBus(const transport::Bus* bus);
 
-serialize::RenderSettings GetRenderSettingSerialize(const json::Node& render_settings);
+serialize::RenderSettings GetSerializeRenderSettings(const json::Node& render_settings);
 
-serialize::RouterSettings GetRouterSettingSerialize(const json::Node& router_settings);
+serialize::RouterSettings GetSerializeRouterSettings(const json::Node& router_settings);
 
-serialize::Router Serialize(const transport::Router& router);
+serialize::Router SerializeRouter(const transport::Router& router);
 
 std::tuple<transport::Catalogue, renderer::MapRenderer, transport::Router,
     graph::DirectedWeightedGraph<double>, std::map<std::string, graph::VertexId>> Deserialize(std::istream& input);
